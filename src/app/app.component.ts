a9a7adf143448704c243042d3e64b3139c8af4e6
renato albuquerque <renatomalb83@gmail.com>
2024-01-04 00:08:35 -0300

    commit all training

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,3 +12,20 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'aula3';
 }
+
+/* 
+1ª CRIAR UM PROJETO: ng new aula03
+
+2ª CRIAR DOIS COMPONENTES:
+ng g c paginas/login
+ng g c paginas/admin
+
+3ª CRIAR O AUTENTICADOR DE ROTAS (AUTH GUARD)
+ng g g seguranca/autenticar
+ng generate guard seguranca/autenticar
+
+4ª BAIXAR OS ARQUIVOS BOOTSTRAP: 
+npm i bootstrap
+*/ 
+
+
